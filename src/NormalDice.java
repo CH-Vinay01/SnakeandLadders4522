7e5561e21b3d3bd0c95c45fcfb2d7d7a6ba77184
CH-Vinay01 <chvinayreddyabc@gmail.com>
2024-12-10 21:05:22 +0530

    Update NormalDice.java

--- src/NormalDice.java
+++ src/NormalDice.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class NormalDice {
+import java.util.Random;
+
+public class NormalDice implements Dice{
+    private int value;
+
+
+    public int get(){
+        Random ran = new Random();
+        int val = ran.nextInt(6) + 1;
+        return val;
+    }
+
+    @Override
+    public void setValue() {
+        this.value += roll();
+    }
+
+    public int roll(){
+        int a = get();
+        if(a == 6){
+            a+=get();
+            if(a == 12){
+                a+=get();
+                if(a == 18){
+                    a=0;
+                }
+            }
+        }
+        return a;
+    }
+
+    @Override
+    public int getValue() {
+        return value;
+    }
 }
