981c1786755c22311a965235a2deff30e04b3989
CH-Vinay01 <chvinayreddyabc@gmail.com>
2024-12-10 21:04:14 +0530

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Game {
+import java.util.HashMap;
+import java.util.Queue;
+
+public class Game {
+    private int n;
+    private Queue<String> players;
+    public Game(int n , Queue<String> players){
+        this.n = n;
+        this.players = players;
+    }
+
 }
