02cad5b917b9257d162ddad28ba151bb2f69063d
CH-Vinay01 <chvinayreddyabc@gmail.com>
2024-12-10 21:02:18 +0530

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Board {
+import java.util.HashMap;
+import java.util.Queue;
+
+public class Board {
+    private int n;
+    private Queue<Integer> players;
+
+    public Board(int n,Queue<Integer> players){
+        this.n = n;
+        this.players = players;
+    }
+
+    public void displayScore(HashMap<String,Integer> ls){
+        System.out.println(players);
+        System.out.println(ls);
+    }
 }
