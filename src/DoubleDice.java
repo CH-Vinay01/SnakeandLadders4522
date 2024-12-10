c9a2b506384b8a75d01d3d24544fa2a49637639d
CH-Vinay01 <chvinayreddyabc@gmail.com>
2024-12-10 21:03:51 +0530

    Update DoubleDice.java

--- src/DoubleDice.java
+++ src/DoubleDice.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class DoubleDice {
+import java.util.Random;
+
+public class DoubleDice implements Dice{
+    private int value;
+
+
+    public int get() {
+        Random ran = new Random();
+        value = ran.nextInt(12) + 1;
+        return value+=roll();
+    }
+
+    @Override
+    public void setValue() {
+        this.value += roll();
+    }
+
+    public int roll(){
+        int a = get();
+        if(a == 12){
+            a+=get();
+            if(a == 24){
+                a+=get();
+                if(a == 36){
+                    a=0;
+                }
+            }
+        }
+        return a;
+    }
+    @Override
+    public int getValue() {
+        return value;
+    }
 }
